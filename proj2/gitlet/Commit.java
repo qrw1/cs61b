064b00b02ca160d6ccb9db52aaf6900f16ad5c58
qrw1 <2049648373@qq.com>
2024-04-10 15:01:13 +0800

    pro2 1

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,171 +2,25 @@ package gitlet;
 
 // TODO: any imports you need here
 
-import java.io.File;
-import java.io.Serializable;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date; // TODO: You'll likely use this in this class
 
-import static gitlet.MyUtils.*;
-import static gitlet.Utils.readObject;
-import static gitlet.Utils.sha1;
-
-
-public class Commit implements Serializable {
-
+/** Represents a gitlet commit object.
+ *  TODO: It's a good idea to give a description here of what else this Class
+ *  does at a high level.
+ *
+ *  @author TODO
+ */
+public class Commit {
     /**
-     * The created date.
-     */
-    private final Date date;
-
-    /**
-     * The message of this Commit.
-     */
-    private final String message;
-
-    /**
-     * The parent commits SHA1 id.
-     */
-    private final List<String> parents;
-
-    /**
-     * The tracked files Map with file path as key and SHA1 id as value.
-     */
-    private final Map<String, String> tracked;
-
-    /**
-     * The SHA1 id.
-     */
-    private final String id;
-
-    /**
-     * The file of this instance with the path generated from SHA1 id.
-     */
-    private final File file;
-
-    public Commit(String message, List<String> parents, Map<String, String> trackedFilesMap) {
-        date = new Date();
-        this.message = message;
-        this.parents = parents;
-        this.tracked = trackedFilesMap;
-        id = generateId();
-        file = getObjectFile(id);
-    }
-
-    /**
-     * Initial commit.
-     */
-    public Commit() {
-        date = new Date(0);
-        message = "initial commit";
-        parents = new ArrayList<>();
-        tracked = new HashMap<>();
-        id = generateId();
-        file = getObjectFile(id);
-    }
-
-    /**
-     * Get a Commit instance from the file with the SHA1 id.
+     * TODO: add instance variables here.
      *
-     * @param id SHA1 id
-     * @return Commit instance
+     * List all instance variables of the Commit class here with a useful
+     * comment above them describing what that variable represents and how that
+     * variable is used. We've provided one example for `message`.
      */
-    public static Commit fromFile(String id) {
-        return readObject(getObjectFile(id), Commit.class);
-    }
 
-    /**
-     * Generate a SHA1 id from timestamp, message, parents Array and tracked files Map.
-     *
-     * @return SHA1 id
-     */
-    private String generateId() {
-        return sha1(getTimestamp(), message, parents.toString(), tracked.toString());
-    }
+    /** The message of this Commit. */
+    private String message;
 
-    /**
-     * Save this Commit instance to file in objects folder.
-     */
-    public void save() {
-        saveObjectFile(file, this);
-    }
-
-    /**
-     * Get the Date instance when the commit is created.
-     *
-     * @return Date instance
-     */
-    public Date getDate() {
-        return date;
-    }
-
-    /**
-     * Get the timestamp.
-     *
-     * @return Date and time
-     */
-    public String getTimestamp() {
-        // Thu Jan 1 00:00:00 1970 +0000
-        DateFormat dateFormat = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z", Locale.ENGLISH);
-        return dateFormat.format(date);
-    }
-
-    /**
-     * Get the commit message.
-     *
-     * @return Commit message
-     */
-    public String getMessage() {
-        return message;
-    }
-
-    /**
-     * Get the parent commit ids.
-     *
-     * @return Array of parent commit ids.
-     */
-    public List<String> getParents() {
-        return parents;
-    }
-
-    /**
-     * Get the tracked files Map with file path as key and SHA1 id as value.
-     *
-     * @return Map with file path as key and SHA1 id as value
-     */
-    public Map<String, String> getTracked() {
-        return tracked;
-    }
-
-
-    /**
-     * Get the SHA1 id.
-     *
-     * @return SHA1 id
-     */
-    public String getId() {
-        return id;
-    }
-
-    /**
-     * Get the commit log.
-     *
-     * @return Log content
-     */
-    public String getLog() {
-        StringBuilder logBuilder = new StringBuilder();
-        logBuilder.append("===").append("\n");
-        logBuilder.append("commit").append(" ").append(id).append("\n");
-        if (parents.size() > 1) {
-            logBuilder.append("Merge:");
-            for (String parent : parents) {
-                logBuilder.append(" ").append(parent, 0, 7);
-            }
-            logBuilder.append("\n");
-        }
-        logBuilder.append("Date:").append(" ").append(getTimestamp()).append("\n");
-        logBuilder.append(message).append("\n");
-        return logBuilder.toString();
-    }
+    /* TODO: fill in the rest of this class. */
 }
--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,100 +1,24 @@
 package gitlet;
 
-
-import static gitlet.MyUtils.exit;
-
-
-
+/** Driver class for Gitlet, a subset of the Git version-control system.
+ *  @author TODO
+ */
 public class Main {
 
-    public static void main(String[] args){
-        if (args.length == 0) {
-            exit("Please enter a command.");
-        }
-
+    /** Usage: java gitlet.Main ARGS, where ARGS contains
+     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
+     */
+    public static void main(String[] args) {
+        // TODO: what if args is empty?
         String firstArg = args[0];
-        switch (firstArg) {
-            case "init" -> {
-                validateNumArgs(args, 1);
-                Repository.init();
-            }
-            case "add" -> {
-                Repository.checkWorkingDir();
-                validateNumArgs(args, 2);
-                String fileName = args[1];
-                new Repository().add(fileName);
-            }
-            case "commit" -> {
-                Repository.checkWorkingDir();
-                validateNumArgs(args, 2);
-                String message = args[1];
-                if (message.length() == 0) {
-                    exit("Please enter a commit message.");
-                }
-                new Repository().commit(message);
-            }
-            case "rm" -> {
-                Repository.checkWorkingDir();
-                validateNumArgs(args, 2);
-                String fileName = args[1];
-                new Repository().remove(fileName);
-            }
-            case "log" -> {
-                Repository.checkWorkingDir();
-                validateNumArgs(args, 1);
-                new Repository().log();
-            }
-            case "global-log" -> {
-                Repository.checkWorkingDir();
-                validateNumArgs(args, 1);
-                Repository.globalLog();
-            }
-            case "find" -> {
-                Repository.checkWorkingDir();
-                validateNumArgs(args, 2);
-                String message = args[1];
-                if (message.length() == 0) {
-                    exit("Found no commit with that message.");
-                }
-                Repository.find(message);
-            }
-            case "status" -> {
-                Repository.checkWorkingDir();
-                validateNumArgs(args, 1);
-                new Repository().status();
-            }
-            case "checkout" -> {
-                Repository.checkWorkingDir();
-                Repository repository = new Repository();
-                switch (args.length) {
-                    case 3 -> {
-                        if (!args[1].equals("--")) {
-                            exit("Incorrect operands.");
-                        }
-                        String fileName = args[2];
-                        repository.checkout(fileName);
-                    }
-                    case 4 -> {
-                        if (!args[2].equals("--")) {
-                            exit("Incorrect operands.");
-                        }
-                        String commitId = args[1];
-                        String fileName = args[3];
-                        repository.checkout(commitId, fileName);
-                    }
-                    case 2 -> {
-                        String branch = args[1];
-                        repository.checkoutBranch(branch);
-                    }
-                    default -> exit("Incorrect operands.");
-                }
-            }
-        }
-    }
-
-    private static void validateNumArgs(String[] args, int n) {
-        if (args.length != n) {
-            exit("Incorrect operands.");
+        switch(firstArg) {
+            case "init":
+                // TODO: handle the `init` command
+                break;
+            case "add":
+                // TODO: handle the `add [filename]` command
+                break;
+            // TODO: FILL THE REST IN
         }
     }
 }
